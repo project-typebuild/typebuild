6ce3f47aecfaf8f2b0d090e1428d557bfbd88a7a
Ranu <neneranadheer@gmail.com>
2023-09-24 08:15:49 +0100

    added CONTRIBUTING.md file

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name="typebuild",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     install_requires=requirements,
     author="iRanadheer",
