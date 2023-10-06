7efda83eaf4a3a2848e120f28702aa9f4fe6fc41
Vivek Desktop <vivekdse+desktop@gmail.com>
2023-10-06 16:01:46 -0700

    Created agents.py to start agent based approach

---
+++ typebuild/agents.py
@@ -0,0 +1,16 @@
+"""
+A list of agents who coordinate with each other
+and the user to build things.
+"""
+
+import os
+import pandas as pd
+from helpers import get_approved_libraries
+import streamlit as st
+
+def coordinator():
+    """
+    This is the main agent that coordinates all the other agents.
+    """
+
+    return None
