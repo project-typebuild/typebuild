7ef07b7807196dd7eea8bcff68a836608535031e
Ranu <neneranadheer@gmail.com>
2023-11-03 02:16:44 +0000

    getting ready for .pkg

--- typebuild/app.py
+++ typebuild/app.py
@@ -3,4 +3,8 @@ import subprocess
 
 def run():
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    subprocess.call(['streamlit', 'run', os.path.join(dir_path, 'main.py')])
+    main_script_path = os.path.join(dir_path, 'main.py')
+    subprocess.call(['streamlit', 'run', main_script_path])
+
+if __name__ == '__main__':
+    run()
