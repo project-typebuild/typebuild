811f241734665473819ec5f8aeb427e3eafc0ce3
Vivek Desktop <vivekdse+desktop@gmail.com>
2023-11-14 17:33:32 -0800

    cleaned up the chat framework

--- typebuild/chat_framework.py
+++ typebuild/chat_framework.py
@@ -6,18 +6,29 @@ import altair as alt
 from test import test_main
 
 class ChatFramework:
-    ask_llm = False
-    # Create a container for the chat messages
-    messages = []
-    display_expanded = False
-    available_methods = ['show_chart', 'chat_input_method', 'display_messages']
-    show_methods = ['show_chart', 'chat_input_method', 'display_messages']
-
+    """A class to manage the chat framework
+    TypeBuild could have different chats going on for different projects, etc.
+    Each instance will hold the data for that particular chat.
+    """
     def __init__(self):
-        return None
+        
+        # At the end of the loop, this determines if the LLM will be called
+        self.ask_llm = False
+        
+        # Create a container for the chat messages
+        self.messages = []
+        
+        # Expand or collaps the display container
+        self.display_expanded = False
 
     def set_user_message(self, message):
         self.messages.append({'role': 'user', 'content': message})
+
+    def set_assistant_message(self, message):
+        self.messages.append({'role': 'assistant', 'content': message})
+
+    def set_system_message(self, message):
+        self.messages.append({'role': 'system', 'content': message})
 
     def display_messages(self):
         # Display the user and assistant messages
@@ -30,56 +41,14 @@ class ChatFramework:
                         with st.chat_message(msg['role']):
                             st.markdown(the_content)
         return None
-    
+        
     def chat_input_method(self):
         prompt = st.chat_input("Enter your message", key="chat_input")
         if prompt:
-            self.messages.append({'role': 'user', 'content': prompt})
+            self.set_user_message(prompt)
             # Set ask llm to true
             self.ask_llm = True
             self.display_expanded = True
         return None
 
-    # Create a random altair chart
-    def show_chart(self):
-        df = pd.DataFrame(
-            [[1, 5, 2], [2, 4, 3], [3, 3, 4], [4, 2, 5], [5, 1, 6]],
-            columns=['a', 'b', 'c'])
-        c = alt.Chart(df).mark_circle().encode(
-            x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-        st.altair_chart(c)
-
-        return None
-
-    def dynamic_menu(self):
-        for method in self.show_methods:
-            getattr(self, method)()
-        # For hidden methods, give a button to toggle them on the sidebar
-        for method in self.available_methods:
-            if method not in self.show_methods:
-                st.info(self.show_methods)
-                if st.sidebar.button(f"Open {method}"):
-                    self.add_to_menu(method)
-        # For shown methods, give a button to toggle them off the sidebar
-        for method in self.show_methods:
-            if method in self.available_methods:
-                if st.sidebar.button(f"Close {method}"):
-                    self.remove_from_menu(method)
-        return None
-
-    def remove_from_menu(self, method):
-        if method in self.show_methods:
-            self.show_methods.remove(method)
-            st.rerun()
-    
-    def add_to_menu(self, method):
-        if method not in self.show_methods:
-            self.show_methods.append(method)
-            st.rerun()
-
-if 'a' not in st.session_state:
-    a = ChatFramework()
-    st.session_state['a'] = a
-else:
-    a = st.session_state['a']
 
