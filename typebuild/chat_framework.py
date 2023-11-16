0f09acf30a91a996a16ce74a743bae1b8554a683
Vivek Desktop <vivekdse+desktop@gmail.com>
2023-11-16 05:59:50 -0800

    Documenting the chatframework with gpt. Did not review output

--- typebuild/chat_framework.py
+++ typebuild/chat_framework.py
@@ -3,33 +3,64 @@ import pandas as pd
 import altair as alt
 
 class ChatFramework:
-    """A class to manage the chat framework
-    TypeBuild could have different chats going on for different projects, etc.
-    Each instance will hold the data for that particular chat.
+    """A class to manage the chat framework for TypeBuild.
+    
+    This class enables the management of different chats for various projects.
+    Each instance of the class holds the data for a specific chat, including messages
+    and display settings.
     """
+
     def __init__(self):
-        
-        # At the end of the loop, this determines if the LLM will be called
+        """
+        Initializes the ChatFramework instance.
+
+        Attributes:
+            ask_llm (bool): Determines if the Large Language Model (LLM) should be called at the end of the loop.
+            messages (list): A container for storing chat messages.
+            display_expanded (bool): Flag to expand or collapse the display container.
+        """
         self.ask_llm = False
-        
-        # Create a container for the chat messages
         self.messages = []
-        
-        # Expand or collaps the display container
         self.display_expanded = False
 
     def set_user_message(self, message):
+        """
+        Adds a user message to the chat.
+
+        Args:
+            message (str): The message content from the user.
+        """
         self.messages.append({'role': 'user', 'content': message})
 
     def set_assistant_message(self, message):
+        """
+        Adds an assistant message to the chat.
+
+        Args:
+            message (str): The message content from the assistant.
+        """
         self.messages.append({'role': 'assistant', 'content': message})
 
     def set_system_message(self, message):
+        """
+        Adds a system message to the chat.
+
+        Args:
+            message (str): The message content from the system.
+        """
         self.messages.append({'role': 'system', 'content': message})
 
     def display_messages(self):
-        # Display the user and assistant messages
-        # TODO: Perhaps I should print the messages from the agents using update?
+        """
+        Displays the messages in the chat.
+
+        Utilizes Streamlit's expander and chat_message for displaying messages.
+        This method iterates through the messages list and displays each one based
+        on the role (user, assistant, system).
+
+        Returns:
+            None
+        """
         if self.messages:
             with st.expander("View chat", expanded=self.display_expanded):
                 for i, msg in enumerate(self.messages):
@@ -38,14 +69,21 @@ class ChatFramework:
                         with st.chat_message(msg['role']):
                             st.markdown(the_content)
         return None
-        
+
     def chat_input_method(self):
+        """
+        Handles the input of chat messages.
+
+        This method provides an input field for the user to enter their message.
+        Upon receiving a message, it updates the messages list and sets the
+        `ask_llm` flag to True.
+
+        Returns:
+            None
+        """
         prompt = st.chat_input("Enter your message", key="chat_input")
         if prompt:
             self.set_user_message(prompt)
-            # Set ask llm to true
             self.ask_llm = True
             self.display_expanded = True
         return None
-
-
