ff13ae13df971a4109e9b184a3da591ac9f6d59d
Ranu <neneranadheer@gmail.com>
2023-12-03 00:40:18 +0000

    added display tool

---
+++ typebuild/tools/display_dataframe.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import streamlit as st
+
+
+class Display():
+
+    def __init__(self):
+        pass
+
+    def dataframe(self, file_name):
+        
+        df = pd.read_parquet(file_name)
+
+        st.dataframe(df)
+
+        return None
+
+
+
+def tool_main(file_name):
+    """
+    Given a file name, this tool will display the dataframe.
+
+    Args:
+        file_name (str): The name of the file to display.
+
+    Returns:
+        None
+    """
+    display = Display()
+
+    display.dataframe(file_name)
+
+    res_dict = {
+        'content': "The dataframe has been displayed.",
+        'task_finished': True,
+        'ask_llm': False,
+    }
+    return res_dict
