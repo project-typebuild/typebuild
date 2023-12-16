a2c758591f2e193814882096be12336dacbbb17e
Vivek Desktop <vivekdse+desktop@gmail.com>
2023-12-16 04:09:30 -0800

    Revert "Updated LLMs for new openai library."
    This reverts commit 810da083dc2f4fe2568d415fc1cd07f991d68014.

--- typebuild/plugins/llms.py
+++ typebuild/plugins/llms.py
@@ -1,7 +1,7 @@
 import os
 import re
 import streamlit as st
-from openai import OpenAI
+import openai
 from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 from tenacity import (
     retry,
@@ -9,25 +9,15 @@ from tenacity import (
     wait_random_exponential,
 ) 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-import json
 
 import sys
 sys.path.append(st.session_state.typebuild_root)
 
-from extractors import Extractors
-
 def last_few_messages(messages):
     """
-    Returns the last few messages from the given list of messages.
-
-    Long messages get rejected by the LLM. So, this function keeps the system message, which is the first message,
-    and the last 3 user and system messages.
-
-    Args:
-        messages (list): A list of messages.
-
-    Returns:
-        list: The last few messages from the given list.
+    Long messages get rejected by the LLM.  So,
+    - Will keep the system message, which is the first message
+    - Will keep the last 3 user and system messages
     """
     last_messages = []
     if messages:
@@ -38,37 +28,34 @@ def last_few_messages(messages):
     last_messages.extend(user_assistant_messages[-7:])
     return last_messages
 
-def get_llm_output(messages, max_tokens=2500, temperature=0.4, model='gpt-4', functions=[]):
-
+def extract_message_to_agent(content):
     """
-    This function retrieves the output from the Language Model (LLM) based on the given messages.
+    Extracts the message to the agent from the content.
+    This is found within <<< and >>>.
+    There will at least be one set of triple angle brackets
+    for this function to be invoked.
+    """
+    pattern = r"<<<([\s\S]*?)>>>"
+    matches = re.findall(pattern, content)
+    if len(matches) == 1:
+        message_to_agent = matches[0].strip()
+    else:
+        message_to_agent = '\n'.join(matches)
+    
+    # Add it to the session state
+    st.session_state.message_to_agent = message_to_agent
+    return message_to_agent
 
-    Args:
-        messages (list): List of messages exchanged between the user and the LLM.
-        max_tokens (int, optional): The maximum number of tokens to generate in the LLM output. Defaults to 2500.
-        temperature (float, optional): The temperature parameter for controlling the randomness of the LLM output. 
-            Higher values (e.g., 1.0) make the output more random, while lower values (e.g., 0.1) make it more deterministic. 
-            Defaults to 0.4.
-        model (str, optional): The name of the LLM model to use. Defaults to 'gpt-4'.
-        functions (list, optional): List of functions to be used by the LLM. Defaults to an empty list.
-
-    Returns:
-        str: The generated output from the LLM.
-
-    Raises:
-        None
-
-    Notes:
-        - If there is a custom_llm.py file in the plugins directory, it will be used instead of the default LLM.
-        - If the 'claude-2' model is requested and available, it will be used instead of the default LLM.
-        - The function may return code or requirements in multiple forms, which need to be extracted separately.
-        - If a function call is received from the LLM, it will be stored in the session state.
+def get_llm_output(messages, max_tokens=2500, temperature=0.4, model='gpt-4', functions=[]):
+    """
+    This checks if there is a custom_llm.py in the plugins directory 
+    If there is, it uses that.
+    If not, it uses the openai llm.
     """
     # Check if there is a custom_llm.py in the plugins directory
     # If there is, use that
     # Get just the last few messages
     messages = last_few_messages(messages)
-    # st.session_state.all_messages.extend(messages)
     st.session_state.last_request = messages
     typebuild_root = st.session_state.typebuild_root
     if os.path.exists(os.path.join(typebuild_root, 'custom_llm.py')):
@@ -79,35 +66,34 @@ def get_llm_output(messages, max_tokens=2500, temperature=0.4, model='gpt-4', fu
     elif model == 'claude-2' and 'claude_key' in st.session_state:
         content = get_claude_response(messages, max_tokens=max_tokens)
     else:
-        content = get_openai_output(messages, max_tokens=max_tokens, temperature=temperature, model=model, functions=functions)
-        # content = msg.get('content', None)
-        # if 'function_call' in msg:
-        #     func_call = msg.get('function_call', None)
-        #     st.session_state.last_function_call = func_call
-        #     st.sidebar.info("Got a function call from LLM")
+        model = 'gpt-3.5-turbo'
+        msg = get_openai_output(messages, max_tokens=max_tokens, temperature=temperature, model=model, functions=functions)
+        content = msg.get('content', None)
+        if 'function_call' in msg:
+            func_call = msg.get('function_call', None)
+            st.session_state.last_function_call = func_call
+            st.sidebar.info("Got a function call from LLM")
+            content = func_call.get('content', None)
     
-    
+    # progress_status.info("Extracting information from response...")
     if content:
         st.session_state.last_response = content
-        # st.session_state.all_messages.append({'role': 'assistant', 'content': content})
     # We can get back code or requirements in multiple forms
     # Look for each form and extract the code or requirements
 
     # Recent GPT models return function_call as a separate json object
     # Look for that first.
-    # If there are triple backticks, we expect code    
-    extractors = Extractors()
+    # If there are triple backticks, we expect code
     if '```' in str(content) or '|||' in str(content):
         # NOTE: THERE IS AN ASSUMPTION THAT WE CAN'T GET BOTH CODE AND REQUIREMENTS
-        extracted, function_name = extractors.extract_func_call_info(content)
+        extracted, function_name = parse_func_call_info(content)
         func_call = {'name': function_name, 'arguments': {'content':extracted}}
         st.session_state.last_function_call = func_call
 
-    import time
-    st.success(f"Response from LLM: {content}")
-    st.session_state.all_messages.append({'role': 'assistant', 'content': content})
-    # st.balloons()
-    # time.sleep(2)
+
+    # Stop ask llm
+    st.session_state.ask_llm = False
+    # progress_status.success('Response generated!')
     return content
 
 
@@ -127,50 +113,30 @@ def get_openai_output(messages, max_tokens=3000, temperature=0.4, model='gpt-4',
     - max_tokens (int): The maximum number of tokens to generate, default 800
     - temperature (float): The temperature for the model. The higher the temperature, the more random the output
     """
-    # Enable adding the key here.
-    api_key = st.session_state.openai_key
-    client = OpenAI(api_key=api_key)
-    if '4' in model:
-        model = "gpt-4-1106-preview"
-    params = {
-        "model": model,
-        "max_tokens": max_tokens,
-        "temperature": temperature,
-        "n": 1,
-        "messages": messages
-    }
     if functions:
-        params['functions'] = functions
+        response = openai.ChatCompletion.create(
+                    model="gpt-4-0613",
+                    messages = messages,
+                    max_tokens=max_tokens,
+                    temperature=temperature,
+                    n=1,
+                    functions=functions,
+                )
+    else:
+        response = openai.ChatCompletion.create(
+                    model=model,
+                    messages = messages,
+                    max_tokens=max_tokens,
+                    temperature=temperature,
+                    n=1,
+                )
+    msg = response.choices[0].message
     
-    # If the word "json" is found in the content, ask for a json object as response
-    json_found = False
-    for i in messages:
-        if "json" in i['content'].lower():
-            json_found = True
-            break
-    if json_found:
-        params['response_format'] = {"type": "json_object"}
-
-    response = client.chat.completions.create(**params)
-    msg = response.choices[0].message.content
-    
+    # Stop ask llm
+    st.session_state.ask_llm = False    
     return msg
 
 def get_claude_response(messages, max_tokens=2000):
-    """
-    Generates a response from the Claude model based on the given messages.
-
-    Args:
-        messages (list): A list of dictionaries representing the conversation messages.
-            Each dictionary should have 'role' and 'content' keys, where 'role' can be
-            either 'assistant' or 'human', and 'content' contains the message content.
-        max_tokens (int, optional): The maximum number of tokens to generate in the response.
-            Defaults to 2000.
-
-    Returns:
-        str: The generated response from the Claude model.
-    """
-
     anthropic = Anthropic(
         api_key=st.session_state.claude_key,
     )
@@ -192,3 +158,85 @@ def get_claude_response(messages, max_tokens=2000):
         max_tokens_to_sample=max_tokens,
     )
     return response.completion
+
+def parse_func_call_info(response):
+    """
+    The LLM can return code or requirements in the content.  
+    Ideally, requirements come in triple pipe delimiters, 
+    but sometimes they come in triple backticks.
+
+    Figure out which one it is and return the extracted code or requirements.
+    """
+    # If there are ```, it could be code or requirements
+    function_name = None
+    if '```' in response:
+        # If it's python code, it should have at least one function in it
+        if 'def ' in response:
+            extracted = parse_code_from_response(response)
+            function_name = 'save_code_to_file'
+        elif 'data_agent' in response:
+            extracted = parse_modified_user_requirements_from_response(response)
+            function_name = 'data_agent'
+        # If it's not python code, it's probably requirements
+        else:
+            extracted = parse_modified_user_requirements_from_response(response)
+            function_name = 'save_requirements_to_file'
+    # If there are |||, it's probably requirements
+    elif '|||' in response:
+        extracted = parse_modified_user_requirements_from_response(response)
+        function_name = 'save_requirements_to_file'
+    else:
+        extracted = None
+    return extracted, function_name
+            
+
+
+
+def parse_code_from_response(response):
+
+    """
+    Returns the code from the response from LLM.
+    In the prompt to code, we have asked the LLM to return the code inside triple backticks.
+
+    Args:
+    - response (str): The response from LLM
+
+    Returns:
+    - matches (list): A list of strings with the code
+
+    """
+
+    pattern = r"```python([\s\S]*?)```"
+    matches = re.findall(pattern, response)
+    if len(matches) > 0:
+        matches = '\n'.join(matches)
+    else:
+        matches = matches[0]
+    return matches
+
+def parse_modified_user_requirements_from_response(response):
+    
+    """
+    Returns the modified user requirements from the response from LLM. 
+    In the prompt to modify, we have asked the LLM to return the modified user requirements inside triple pipe delimiters.
+
+    Args:
+    - response (str): The response from LLM
+
+    Returns:
+    - matches (list): A list of strings with the modified user requirements
+
+    """
+    if '|||' in response:
+        pattern = r"\|\|\|([\s\S]*?)\|\|\|"
+    if '```' in response:
+        # It shouldnt have ```python in it
+        pattern = r"```([\s\S]*?)```"
+
+    matches = re.findall(pattern, response)
+    # if there are multiple matches, join by new line
+    if len(matches) > 0:
+        matches = '\n'.join(matches)
+    else:
+        matches = matches[0]
+    return matches
