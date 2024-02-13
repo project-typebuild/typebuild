1cfed51ef5d072ce16ef698127345b23cbf2f75b
Ranu <neneranadheer@gmail.com>
2024-02-13 22:37:13 +0530

    added audio folder to session state

--- typebuild/object_management.py
+++ typebuild/object_management.py
@@ -3,10 +3,12 @@ import streamlit as st
 def test():
     # file = "/home/vivek/.typebuild/users/vivek/data/arxiv_all_confabulation_hallucination_language_models.parquet"
     # display = Display(file)
-    dynamic_functions = st.session_state.dynamic_functions
-    # Go through all the keys and dynamically call the values
-    for key in dynamic_functions:
-        the_function = dynamic_functions[key].get('func')
-        if the_function:
-            the_function()
+    # check if dynamic_functions exists
+    if 'dynamic_functions' in st.session_state:
+        dynamic_functions = st.session_state.dynamic_functions
+        # Go through all the keys and dynamically call the values
+        for key in dynamic_functions:
+            the_function = dynamic_functions[key].get('func')
+            if the_function:
+                the_function()
 
